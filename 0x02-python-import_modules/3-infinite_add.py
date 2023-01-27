381b0b5e1382ea3000630ba46a8816adeb8d0f3d
Devjallow <bjallow000@outlook.com>
2023-01-27 12:51:39 +0000

    fixed the error

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-import sys
-print(sum(map(int, sys.argv[1:])))
+if __name__ == " __main__ ":
+    """ print sum of all argument """
+    import sys
+    
+    total = sum(map(int, sys.argv[1:]))
+    print("{}".format(total))
