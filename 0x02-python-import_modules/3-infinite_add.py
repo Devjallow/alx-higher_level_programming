49f3851d8540d9381b9273a2655867622c0e667e
Devjallow <bjallow000@outlook.com>
2023-01-27 12:57:16 +0000

    reduce the code

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,5 +3,4 @@ if __name__ == " __main__ ":
     """ print sum of all argument """
     import sys
     
-    total = sum(map(int, sys.argv[1:]))
-    print("{}".format(total))
+    print("{}".format(sum(map(int, sys.argv[1:]))))
