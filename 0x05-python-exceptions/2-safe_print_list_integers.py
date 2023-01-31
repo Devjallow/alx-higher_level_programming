ab9246bbc1f91d6fc937d7e9994208efb3e80ffd
Bubacarr <bjallow000@outlook.com>
2023-01-30 18:50:24 -0800

    fixed the error

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,5 +8,5 @@ def safe_print_list_integers(my_lsit=[], x=0):
             ret += 1
         except (ValueError, TypeError):
             continue
-        print("")
-        return (ret)
+    print("")
+    return (ret)
