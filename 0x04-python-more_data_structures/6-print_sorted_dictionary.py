7bedfdbe43755d5add7d3d5d713e31fb223f30bb
Bubacarr <bjallow000@outlook.com>
2023-02-08 18:00:58 -0800

    fixed pycodestyle error

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,4 @@ def print_sorted_dictionary(a_dictionary):
     values = list(a_dictionary.items())
     values.sort()
     for k, v in values:
-        print("{}: {}".format(k,v))
+        print("{}: {}".format(k, v))
