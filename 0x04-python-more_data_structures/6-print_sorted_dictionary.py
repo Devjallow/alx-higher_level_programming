0d6d0cc860b3702558a9ecfa54d9d7464c80cc5a
Bubacarr <bjallow000@outlook.com>
2023-02-08 17:56:17 -0800

    sorted dictionary

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    values = list(a_dictionary.items())
+    values.sort()
+    for k, v in values:
+        print("{}: {}".format(k,v))
