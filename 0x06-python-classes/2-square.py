d0537c55f09e50374853dcae28525b5b5ee7bb0f
Bubacarr <bjallow000@outlook.com>
2023-02-11 02:05:34 -0800

    class sqaure with exceptions

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Define a class square """
+
+
+class Square:
+    """ Represent a square """
+    def __int__(self, size):
+        if type(size) != int:
+            raise TypeError("size must be an interger")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
