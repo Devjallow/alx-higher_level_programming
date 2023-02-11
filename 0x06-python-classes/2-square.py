2ea2b141eb1016a9e659e401ccfb48e335264890
Bubacarr <bjallow000@outlook.com>
2023-02-11 02:17:47 -0800

    fixed the error

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,7 +11,7 @@ class Square:
             size(int): size of the new square
         """
             
-        if type(size) != int:
+        if not isinstance(size, int):
             raise TypeError("size must be an interger")
         if size < 0:
             raise ValueError("size must be >= 0")
