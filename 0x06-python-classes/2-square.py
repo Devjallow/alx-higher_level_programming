4f2c02e8ec824f04cce6067437d2857c2f11f51a
Bubacarr <bjallow000@outlook.com>
2023-02-12 17:45:08 -0800

    modified the string to be printed

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,7 +11,7 @@ class Square:
             size (int): represent the size of a the sqaure
         """
         if not isinstance(size, int):
-            raise TypeError("size must be an interger")
+            raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
