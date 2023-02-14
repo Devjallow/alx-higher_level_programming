b8f91ce5c8e85741d3898468df43ccd0c641a112
Bubacarr <bjallow000@outlook.com>
2023-02-14 07:29:27 -0800

    modified the code

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -16,11 +16,15 @@ class Square:
         """
         if type(size) != int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(size):
-        """Return the area of size"""
+        """
+        Calculate the area of a square
 
+        Return:
+            area of a square
+        """
         return self.__size ** 2
