6fb5f735fa96608278688486cd1b7567542e0724
Bubacarr <bjallow000@outlook.com>
2023-02-14 06:36:32 -0800

    fixed the error

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,14 +3,20 @@
 
 
 class Square:
-    """Represent a square
-
-    Args:
-        size(int) : the size of the square
-    """
+    """Represent a square"""
     def __init__(self, size=0):
+        """
+        Initialize a new square object
+
+        Args:
+            size(int): size of the square.
+
+        Raise:
+            raise type error if parameter is not int
+            raise value error if parameter is < 0.
+        """
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
-            raise TypeError("size must be >=0")
+            raise ValueError("size must be >=0")
         self.__size = size
