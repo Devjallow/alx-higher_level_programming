e24907a1f7e25f6a1a205de15c1f1ef3d1434713
Bubacarr <bjallow000@outlook.com>
2023-02-22 09:58:27 -0800

    print # square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Define a square """
+
+
+
+class Square:
+    """ Represent a square """
+    def __init__(self, size=0):
+        """
+        Initilizing a square
+
+        Args:
+            size(int): size of a square.
+        Raise:
+            raise a typeerro if value is not integer
+            raise valueerror if value is  less than zero
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """get/setter getting and setting the value"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("value most be integer")
+        elif value < 0:
+            raise ValueError("value must be >= 0")
+        else:
+            return self.__size = value
+    def area(self):
+        """ return area of a square """
+        return self.__size ** 2
+    def my_print(self):
+        """ print square """
+        pass
