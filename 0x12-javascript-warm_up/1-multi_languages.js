7d5f795d5102a49e5f70a3c95384fe7ca80deb26
Devjallow <bjallow000@outlook.com>
2023-04-12 21:22:08 +0000

    print three lines of code

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+let firsLine = 'C is fun'
+let secondline = 'Python is cool'
+let thirdLine = 'javaScript is amazing'
+
+console.log(firlsLine);
+console.log(secondLine);
+console.log(thirdLine);
