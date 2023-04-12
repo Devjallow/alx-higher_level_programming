3b9e0158e2f24e3b2baaef9bee80a5f044924dea
Devjallow <bjallow000@outlook.com>
2023-04-12 21:26:10 +0000

    fixed the error

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
 
-let firsLine = 'C is fun'
-let secondline = 'Python is cool'
-let thirdLine = 'javaScript is amazing'
-
-console.log(firlsLine);
-console.log(secondLine);
-console.log(thirdLine);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
