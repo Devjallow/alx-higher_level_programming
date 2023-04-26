ef894028c5137f30d713bf52d5f44023ec763bfb
Devjallow <bjallow000@outlook.com>
2023-04-26 00:43:02 +0000

    added the files

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const arg = process.argv.slice(2);
+if (process.argv.length === 2){
+	console.log('Missing number of occurrences');
+}
+for (let i = 0; i < arg; i++){
+	console.log('C is fun');
+}
